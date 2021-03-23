4f0db90f1d70c8930b50ffb64d1f7ec9db570182
GarrettU27 <udstr013@umn.edu>
2021-03-23 09:47:55 -0500

    Create an interface. We can include all our libraries in one header file and use them within main

---
+++ projectLibrary.h
@@ -0,0 +1,26 @@
+#include "iLED.h"
+#include "ADC.h"
+
+/* 
+ * File:   projectLibrary.h
+ * Author: garre
+ *
+ * Created on March 23, 2021, 9:46 AM
+ */
+
+#ifndef PROJECTLIBRARY_H
+#define	PROJECTLIBRARY_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PROJECTLIBRARY_H */
+
