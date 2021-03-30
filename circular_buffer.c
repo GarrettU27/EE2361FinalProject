888e4e85e0c50df0cfb1af52bf4c58ebb012f378
GarrettU27 <udstr013@umn.edu>
2021-03-30 11:26:44 -0500

    Added in better logic for circular buffer averaging, so now the buffer can get pretty big without much change in the time to calculate the average. Updated .gitignore to remove dist and build directories. May be too little too late. Also changed the buffer size to 1024, makes it stay lower and have less flickering, but also keeps it higher longer, so the light sustains a bit

--- circular_buffer.c
+++ circular_buffer.c
@@ -1,9 +1,10 @@
 #include "circular_buffer.h"
 
-#define BUFFER_SIZE 128
+#define BUFFER_SIZE 1024
 
 int avg = 0;
 int tail = 0;
+unsigned long int sum = 0;
 int buffer[BUFFER_SIZE];
 
 void initBuffer(void) {
@@ -15,23 +16,16 @@ void initBuffer(void) {
     }
 }
 
-void updateAvg() {
-    int index = 0;
-    unsigned long int sum = 0;
-    while (index < BUFFER_SIZE) {
-        sum += buffer[index];
-        index++;
-    }
-    avg = sum/BUFFER_SIZE;
-}
-
 int getAvg() {
     return avg;
 }
 
 void putVal(int newValue) {
+    //remove old value, add in new value and update the average
+    sum -= buffer[tail];
+    sum += newValue;
+    avg = sum/BUFFER_SIZE;
     buffer[tail] = newValue;
-    updateAvg();
     tail = (tail+1) % BUFFER_SIZE;
 }
 
